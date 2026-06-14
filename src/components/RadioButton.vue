2451dd83f19d2a1ce25bc55e2bb6c9cc9e218d0e
akariaka@yandex.ru <akariaka@yandex.ru>
2026-06-14 18:19:50 +0600

    create component

---
+++ src/components/RadioButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  import { changeValue } from '../constants';
+
+  const props = defineProps({
+    title: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: String,
+      default: ''
+    },
+    checked: {
+      type: Boolean
+    }
+  })
+</script>
+
+<template>
+  <div class="cells__type ">
+    <label :for="value">{{ title }}</label>
+    <input name="change" :value="value" :id="value" type="radio" v-model="changeValue" :checked="checked" />
+  </div>
+</template>
