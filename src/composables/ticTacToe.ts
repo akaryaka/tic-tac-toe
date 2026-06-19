d29d32187cc50aefe78fcf5bd2d992bedecc1830
unknown <akariaka@yandex.ru>
2026-06-19 17:44:25 +0600

    begin wincheck dev

--- src/composables/ticTacToe.ts
+++ src/composables/ticTacToe.ts
@@ -1,37 +1,61 @@
-import { countClick, changeValue, buttons, displayFirstStep, displaySecondStep } from "../constants";
+import { watch } from "vue";
+import {
+  countClick,
+  changeValue,
+  buttons,
+  displayFirstStep,
+  displaySecondStep,
+  winCheck,
+  btnActive,
+} from "../constants";
 
-export const btnClick = function(event: any, item: any) {
+export const btnClick = function (event: Event, item: any) {
   event.preventDefault();
-  
+
   item.state = !item.state;
-  item.classDisabled = 'disabled';
+
+  item.classDisabled = "disabled";
   countClick.value++;
 
-  if (changeValue.value=='round' && countClick.value%2==1) {
-    item.class = 'round';
-  } else if (changeValue.value=='round' && countClick.value%2==0) {
-    item.class = 'cross';
-  } else if (changeValue.value=='cross' && countClick.value%2==1) {
-    item.class = 'cross';
-  } else  if (changeValue.value=='cross' && countClick.value%2==0) {
-    item.class = 'round';
+  if (changeValue.value == "round" && countClick.value % 2 == 1) {
+    item.class = "round";
+  } else if (changeValue.value == "round" && countClick.value % 2 == 0) {
+    item.class = "cross";
+  } else if (changeValue.value == "cross" && countClick.value % 2 == 1) {
+    item.class = "cross";
+  } else if (changeValue.value == "cross" && countClick.value % 2 == 0) {
+    item.class = "round";
   }
+  // console.log(item.class);
 
-  event.target.classList.add(item.class);
-}
+  // event.currentTarget.classList.add(item.class);
+  // console.log(event.target);
 
-export const reset = function() {
+  btnActive.push(item);
+};
+
+watch(btnActive, (oldvalue, newvalue) => {
+  console.log(btnActive);
+
+  // let btnActiveArr = Object.values(btnActive);
+  // const check = winCheck.some(
+  // (arr) => JSON.stringify(arr) === JSON.stringify(btnActiveArr),
+  // );
+  // console.log(check);
+});
+
+export const reset = function () {
   countClick.value = 0;
-  
+
   buttons.forEach((item) => {
     item.state = false;
-    item.class = '';
-    item.classDisabled = '';
-  })
-}
+    item.class = "";
+    item.classDisabled = "";
+  });
+};
 
-export const toSecondStep = function(event: Event) {
-  event.preventDefault()
+export const toSecondStep = function (event: Event) {
+  event.preventDefault();
   displayFirstStep.value = false;
   displaySecondStep.value = true;
-}
+};
